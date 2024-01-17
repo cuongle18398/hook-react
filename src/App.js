1231a9ac0850d1dbc07db0fec8033309eff93ee7
Cuong Le <cuongle18398@gmail.com>
2024-01-17 21:29:56 +0700

    "Update 17/1"

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Nav from './views/Nav';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [name, setName] = useState('Eric')
+
+  const [todos, setTodo] = useState([
+    { id: 'todo1', title: 'Watching TV' },
+    { id: 'todo2', title: 'Doing Homework' },
+    { id: 'todo3', title: 'Playing Game' }
+  ])
+
+  const handleEventClick = () => {
+    setName('Cuong');
+  }
+
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button type="button" onClick={() => handleEventClick()}> Click Me </button>
+        <div className='todo-container'>
+          {todos && todos.length > 0 &&
+            todos.map((item, index) => {
+              return (
+                <div>
+                  <li className='todo-child' key={item.id}>{item.id} - {item.title}</li>
+                </div>
+              )
+            })
+          }
+        </div>
       </header>
     </div>
   );
